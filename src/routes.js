cb56e8f684b2833de842d5a0f5ffd7fd5ba62287
upalr <shovon.forums@gmail.com>
2018-03-06 15:48:11 +1100

    import route chank

--- src/routes.js
+++ src/routes.js
@@ -11,9 +11,11 @@ var Redirect = Router.Redirect;
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/homePage')} />
+
         <Route name="authors" handler={require('./components/authors/authorPage')} />
         <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
         <Route name="manageAuthor" path="author/:id" handler={require('./components/authors/manageAuthorPage')} />
+        
         <Route name="about" handler={require('./components/about/aboutPage')} />
         <NotFoundRoute handler={require('./components/notFoundPage')} />
         <Redirect from="about-us" to="about" />
